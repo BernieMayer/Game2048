6771bc9173e20f55b73a5df5e38d41c239a57b38
bemayer <bemayer@ucalgary.ca>
2021-02-26 13:18:16 -0700

    The board will now generate the inital 2 pieces

--- src/Game2048.java
+++ src/Game2048.java
@@ -1,14 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.SplittableRandom;
 
 public class Game2048 {
-    int[][] gameBoard;
+    int[][] gameBoard;  //use 0 to indicate that a position on the board is empty
+
+    final int GAME_BOARD_WIDTH = 4;
+    final int GAME_BOARD_HEIGHT = 4;
+
+
+    SplittableRandom random;
 
 
     public Game2048() {
-        gameBoard = new int[4][4];
+        gameBoard = new int[GAME_BOARD_WIDTH][GAME_BOARD_HEIGHT];
+        random = new SplittableRandom();
 
-        gameBoard[0][0] = 2;
-        gameBoard[0][1] = 4;
+        //add the initial two tiles to the gameboard
+        addTileToGameBoard();
+        addTileToGameBoard();
+    }
+
+    private void addTileToGameBoard() {
+        ArrayList<int[]> emptyBoardSpaces = new ArrayList<>();
+        for (int i = 0; i < GAME_BOARD_WIDTH; i++) {
+            for (int j = 0; j < GAME_BOARD_HEIGHT; j++) {
+                if (gameBoard[i][j] == 0) {
+                    emptyBoardSpaces.add(new int[]{i, j});
+                }
+            }
+        }
+
+        Collections.shuffle(emptyBoardSpaces);
+
+        if (gameBoard.length > 0) {
+            gameBoard[emptyBoardSpaces.get(0)[0]][emptyBoardSpaces.get(0)[1]] = generateTileNumber();
+        }
+    }
+
+    private int generateTileNumber() {
+        int result = random.nextInt(1, 11);
+
+        //90%  of the time we want the result to be 2
+        if (result <= 9) {
+            return 2;
+        } else {
+            return 4;
+        }
     }
 
     public int[][] getGameBoard() {
