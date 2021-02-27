8a11f8b8e8ea67ed98ba5bfa99f27992c0307613
bemayer <bemayer@ucalgary.ca>
2021-02-26 23:47:12 -0700

    Fix merge conflict issues

--- tests/Game2048Test.java
+++ tests/Game2048Test.java
@@ -165,64 +165,6 @@ public class Game2048Test {
 
    }
 
-   @Test
-   public void testSimpleMoveLeftLogic() {
-
-      int[][] testBoard = new int[][]{ {2, 2, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-
-      Game2048 game2048 = new Game2048();
-      Game2048.TestAccessor testAccessor = game2048.getTestAccessor();
-
-      testAccessor.setGameBoard(testBoard);
-
-      game2048.makeMove(Game2048.Move.LEFT);
-
-      assertThat(testBoard[0][0], CoreMatchers.is(4));
-   }
-
-   @Test
-   public void testSimpleMoveRightLogic() {
-      int[][] testBoard = new int[][]{ {2, 2, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-
-      Game2048 game2048 = new Game2048();
-      Game2048.TestAccessor testAccessor = game2048.getTestAccessor();
-
-      testAccessor.setGameBoard(testBoard);
-
-      game2048.makeMove(Game2048.Move.RIGHT);
-
-      assertThat(testBoard[0][0], CoreMatchers.is(4));
-   }
-
-   @Test
-   public void testSimpleMoveDownLogic() {
-      int[][] testBoard = new int[][]{ {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 2}, {0, 0, 0, 2}};
-
-      Game2048 game2048 = new Game2048();
-      Game2048.TestAccessor testAccessor = game2048.getTestAccessor();
-
-      testAccessor.setGameBoard(testBoard);
-
-      game2048.makeMove(Game2048.Move.DOWN);
-
-      assertThat(testBoard[3][3], CoreMatchers.is(4));
-
-   }
-
-   @Test
-   public void testSimpleMoveUpLogic() {
-      int[][] testBoard = new int[][]{ {0, 0, 0, 2}, {0, 0, 0, 2}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-
-      Game2048 game2048 = new Game2048();
-      Game2048.TestAccessor testAccessor = game2048.getTestAccessor();
-
-      testAccessor.setGameBoard(testBoard);
-
-      game2048.makeMove(Game2048.Move.UP);
-
-      assertThat(testBoard[0][3], CoreMatchers.is(4));
-
-   }
 
    @Test
    public void testGameOverWinningFor2048() {
@@ -235,8 +177,4 @@ public class Game2048Test {
 
       assertThat(game2048.isGameOver(), CoreMatchers.is(true));
    }
-
-
-
-
 }
